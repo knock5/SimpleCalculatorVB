a691bfe2a8316664a0a82eb322ae173f0f893826
knock5 <miftachulhd5@gmail.com>
2022-09-15 20:58:47 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TxtHasil = New System.Windows.Forms.TextBox()
+        Me.BtnTambah = New System.Windows.Forms.Button()
+        Me.BtnKurang = New System.Windows.Forms.Button()
+        Me.BtnKali = New System.Windows.Forms.Button()
+        Me.BtnEnam = New System.Windows.Forms.Button()
+        Me.BtnDelapan = New System.Windows.Forms.Button()
+        Me.BtnSatu = New System.Windows.Forms.Button()
+        Me.BtnDua = New System.Windows.Forms.Button()
+        Me.BtnTiga = New System.Windows.Forms.Button()
+        Me.BtnTujuh = New System.Windows.Forms.Button()
+        Me.BtnSembilan = New System.Windows.Forms.Button()
+        Me.BtnEmpat = New System.Windows.Forms.Button()
+        Me.BtnLima = New System.Windows.Forms.Button()
+        Me.BtnKosong = New System.Windows.Forms.Button()
+        Me.BtnBagi = New System.Windows.Forms.Button()
+        Me.BtnEquals = New System.Windows.Forms.Button()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.BtnClearEntry = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtHasil
+        '
+        Me.TxtHasil.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TxtHasil.Font = New System.Drawing.Font("Poppins Medium", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TxtHasil.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TxtHasil.Location = New System.Drawing.Point(27, 31)
+        Me.TxtHasil.Name = "TxtHasil"
+        Me.TxtHasil.Size = New System.Drawing.Size(398, 63)
+        Me.TxtHasil.TabIndex = 0
+        Me.TxtHasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BtnTambah
+        '
+        Me.BtnTambah.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnTambah.ForeColor = System.Drawing.Color.White
+        Me.BtnTambah.Location = New System.Drawing.Point(330, 351)
+        Me.BtnTambah.Name = "BtnTambah"
+        Me.BtnTambah.Size = New System.Drawing.Size(95, 64)
+        Me.BtnTambah.TabIndex = 1
+        Me.BtnTambah.Text = "+"
+        Me.BtnTambah.UseVisualStyleBackColor = False
+        '
+        'BtnKurang
+        '
+        Me.BtnKurang.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnKurang.ForeColor = System.Drawing.Color.White
+        Me.BtnKurang.Location = New System.Drawing.Point(330, 281)
+        Me.BtnKurang.Name = "BtnKurang"
+        Me.BtnKurang.Size = New System.Drawing.Size(95, 64)
+        Me.BtnKurang.TabIndex = 2
+        Me.BtnKurang.Text = "-"
+        Me.BtnKurang.UseVisualStyleBackColor = False
+        '
+        'BtnKali
+        '
+        Me.BtnKali.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnKali.ForeColor = System.Drawing.Color.White
+        Me.BtnKali.Location = New System.Drawing.Point(330, 211)
+        Me.BtnKali.Name = "BtnKali"
+        Me.BtnKali.Size = New System.Drawing.Size(95, 64)
+        Me.BtnKali.TabIndex = 3
+        Me.BtnKali.Text = "X"
+        Me.BtnKali.UseVisualStyleBackColor = False
+        '
+        'BtnEnam
+        '
+        Me.BtnEnam.BackColor = System.Drawing.Color.Black
+        Me.BtnEnam.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnEnam.Location = New System.Drawing.Point(229, 281)
+        Me.BtnEnam.Name = "BtnEnam"
+        Me.BtnEnam.Size = New System.Drawing.Size(95, 64)
+        Me.BtnEnam.TabIndex = 4
+        Me.BtnEnam.Text = "6"
+        Me.BtnEnam.UseVisualStyleBackColor = False
+        '
+        'BtnDelapan
+        '
+        Me.BtnDelapan.BackColor = System.Drawing.Color.Black
+        Me.BtnDelapan.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnDelapan.Location = New System.Drawing.Point(128, 351)
+        Me.BtnDelapan.Name = "BtnDelapan"
+        Me.BtnDelapan.Size = New System.Drawing.Size(95, 64)
+        Me.BtnDelapan.TabIndex = 5
+        Me.BtnDelapan.Text = "8"
+        Me.BtnDelapan.UseVisualStyleBackColor = False
+        '
+        'BtnSatu
+        '
+        Me.BtnSatu.BackColor = System.Drawing.Color.Black
+        Me.BtnSatu.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnSatu.Location = New System.Drawing.Point(27, 209)
+        Me.BtnSatu.Name = "BtnSatu"
+        Me.BtnSatu.Size = New System.Drawing.Size(95, 64)
+        Me.BtnSatu.TabIndex = 6
+        Me.BtnSatu.Text = "1"
+        Me.BtnSatu.UseVisualStyleBackColor = False
+        '
+        'BtnDua
+        '
+        Me.BtnDua.BackColor = System.Drawing.Color.Black
+        Me.BtnDua.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnDua.Location = New System.Drawing.Point(128, 209)
+        Me.BtnDua.Name = "BtnDua"
+        Me.BtnDua.Size = New System.Drawing.Size(95, 64)
+        Me.BtnDua.TabIndex = 7
+        Me.BtnDua.Text = "2"
+        Me.BtnDua.UseVisualStyleBackColor = False
+        '
+        'BtnTiga
+        '
+        Me.BtnTiga.BackColor = System.Drawing.Color.Black
+        Me.BtnTiga.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnTiga.Location = New System.Drawing.Point(229, 209)
+        Me.BtnTiga.Name = "BtnTiga"
+        Me.BtnTiga.Size = New System.Drawing.Size(95, 64)
+        Me.BtnTiga.TabIndex = 8
+        Me.BtnTiga.Text = "3"
+        Me.BtnTiga.UseVisualStyleBackColor = False
+        '
+        'BtnTujuh
+        '
+        Me.BtnTujuh.BackColor = System.Drawing.Color.Black
+        Me.BtnTujuh.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnTujuh.Location = New System.Drawing.Point(27, 351)
+        Me.BtnTujuh.Name = "BtnTujuh"
+        Me.BtnTujuh.Size = New System.Drawing.Size(95, 64)
+        Me.BtnTujuh.TabIndex = 9
+        Me.BtnTujuh.Text = "7"
+        Me.BtnTujuh.UseVisualStyleBackColor = False
+        '
+        'BtnSembilan
+        '
+        Me.BtnSembilan.BackColor = System.Drawing.Color.Black
+        Me.BtnSembilan.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnSembilan.Location = New System.Drawing.Point(229, 351)
+        Me.BtnSembilan.Name = "BtnSembilan"
+        Me.BtnSembilan.Size = New System.Drawing.Size(95, 64)
+        Me.BtnSembilan.TabIndex = 10
+        Me.BtnSembilan.Text = "9"
+        Me.BtnSembilan.UseVisualStyleBackColor = False
+        '
+        'BtnEmpat
+        '
+        Me.BtnEmpat.BackColor = System.Drawing.Color.Black
+        Me.BtnEmpat.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnEmpat.Location = New System.Drawing.Point(27, 281)
+        Me.BtnEmpat.Name = "BtnEmpat"
+        Me.BtnEmpat.Size = New System.Drawing.Size(95, 64)
+        Me.BtnEmpat.TabIndex = 11
+        Me.BtnEmpat.Text = "4"
+        Me.BtnEmpat.UseVisualStyleBackColor = False
+        '
+        'BtnLima
+        '
+        Me.BtnLima.BackColor = System.Drawing.Color.Black
+        Me.BtnLima.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnLima.Location = New System.Drawing.Point(128, 281)
+        Me.BtnLima.Name = "BtnLima"
+        Me.BtnLima.Size = New System.Drawing.Size(95, 64)
+        Me.BtnLima.TabIndex = 12
+        Me.BtnLima.Text = "5"
+        Me.BtnLima.UseVisualStyleBackColor = False
+        '
+        'BtnKosong
+        '
+        Me.BtnKosong.BackColor = System.Drawing.Color.Black
+        Me.BtnKosong.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnKosong.Location = New System.Drawing.Point(27, 421)
+        Me.BtnKosong.Name = "BtnKosong"
+        Me.BtnKosong.Size = New System.Drawing.Size(196, 64)
+        Me.BtnKosong.TabIndex = 13
+        Me.BtnKosong.Text = "0"
+        Me.BtnKosong.UseVisualStyleBackColor = False
+        '
+        'BtnBagi
+        '
+        Me.BtnBagi.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnBagi.ForeColor = System.Drawing.Color.White
+        Me.BtnBagi.Location = New System.Drawing.Point(330, 139)
+        Me.BtnBagi.Name = "BtnBagi"
+        Me.BtnBagi.Size = New System.Drawing.Size(95, 64)
+        Me.BtnBagi.TabIndex = 14
+        Me.BtnBagi.Text = "/"
+        Me.BtnBagi.UseVisualStyleBackColor = False
+        '
+        'BtnEquals
+        '
+        Me.BtnEquals.BackColor = System.Drawing.Color.Lime
+        Me.BtnEquals.ForeColor = System.Drawing.Color.Black
+        Me.BtnEquals.Location = New System.Drawing.Point(229, 421)
+        Me.BtnEquals.Name = "BtnEquals"
+        Me.BtnEquals.Size = New System.Drawing.Size(196, 64)
+        Me.BtnEquals.TabIndex = 15
+        Me.BtnEquals.Text = "="
+        Me.BtnEquals.UseVisualStyleBackColor = False
+        '
+        'BtnClear
+        '
+        Me.BtnClear.BackColor = System.Drawing.Color.Red
+        Me.BtnClear.ForeColor = System.Drawing.Color.Black
+        Me.BtnClear.Location = New System.Drawing.Point(27, 139)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(196, 64)
+        Me.BtnClear.TabIndex = 16
+        Me.BtnClear.Text = "C"
+        Me.BtnClear.UseVisualStyleBackColor = False
+        '
+        'BtnClearEntry
+        '
+        Me.BtnClearEntry.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnClearEntry.ForeColor = System.Drawing.Color.Black
+        Me.BtnClearEntry.Location = New System.Drawing.Point(229, 139)
+        Me.BtnClearEntry.Name = "BtnClearEntry"
+        Me.BtnClearEntry.Size = New System.Drawing.Size(95, 64)
+        Me.BtnClearEntry.TabIndex = 17
+        Me.BtnClearEntry.Text = "CE"
+        Me.BtnClearEntry.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 26.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(453, 508)
+        Me.Controls.Add(Me.BtnClearEntry)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.BtnEquals)
+        Me.Controls.Add(Me.BtnBagi)
+        Me.Controls.Add(Me.BtnKosong)
+        Me.Controls.Add(Me.BtnLima)
+        Me.Controls.Add(Me.BtnEmpat)
+        Me.Controls.Add(Me.BtnSembilan)
+        Me.Controls.Add(Me.BtnTujuh)
+        Me.Controls.Add(Me.BtnTiga)
+        Me.Controls.Add(Me.BtnDua)
+        Me.Controls.Add(Me.BtnSatu)
+        Me.Controls.Add(Me.BtnDelapan)
+        Me.Controls.Add(Me.BtnEnam)
+        Me.Controls.Add(Me.BtnKali)
+        Me.Controls.Add(Me.BtnKurang)
+        Me.Controls.Add(Me.BtnTambah)
+        Me.Controls.Add(Me.TxtHasil)
+        Me.Font = New System.Drawing.Font("Poppins Medium", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Simple Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtHasil As TextBox
+    Friend WithEvents BtnTambah As Button
+    Friend WithEvents BtnKurang As Button
+    Friend WithEvents BtnKali As Button
+    Friend WithEvents BtnEnam As Button
+    Friend WithEvents BtnDelapan As Button
+    Friend WithEvents BtnSatu As Button
+    Friend WithEvents BtnDua As Button
+    Friend WithEvents BtnTiga As Button
+    Friend WithEvents BtnTujuh As Button
+    Friend WithEvents BtnSembilan As Button
+    Friend WithEvents BtnEmpat As Button
+    Friend WithEvents BtnLima As Button
+    Friend WithEvents BtnKosong As Button
+    Friend WithEvents BtnBagi As Button
+    Friend WithEvents BtnEquals As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnClearEntry As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class Form1
+
+    Friend Hasil As Single = 0
+    Dim operasi As String = ""
+    Dim operasiHitung As Boolean = False
+
+
+    Private Sub SimpleCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        operasiHitung = False
+        operasi = ""
+        Hasil = 0
+        TxtHasil.Text = "0"
+
+    End Sub
+
+    Private Sub BtnSatu_Click(sender As Object, e As EventArgs) Handles BtnSatu.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "1"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "1"
+        End If
+
+    End Sub
+
+    Private Sub BtnDua_Click(sender As Object, e As EventArgs) Handles BtnDua.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "2"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "2"
+        End If
+
+    End Sub
+
+    Private Sub BtnTiga_Click(sender As Object, e As EventArgs) Handles BtnTiga.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "3"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "3"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnEmpat_Click(sender As Object, e As EventArgs) Handles BtnEmpat.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "4"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "4"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnLima_Click(sender As Object, e As EventArgs) Handles BtnLima.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "5"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "5"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnEnam_Click(sender As Object, e As EventArgs) Handles BtnEnam.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "6"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "6"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnTujuh_Click(sender As Object, e As EventArgs) Handles BtnTujuh.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "7"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "7"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnDelapan_Click(sender As Object, e As EventArgs) Handles BtnDelapan.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "8"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "2"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnSembilan_Click(sender As Object, e As EventArgs) Handles BtnSembilan.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "9"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "9"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnKosong_Click(sender As Object, e As EventArgs) Handles BtnKosong.Click
+
+        If operasiHitung = True Or TxtHasil.Text = "0" Then
+            TxtHasil.Text = "0"
+            operasiHitung = False
+        Else
+            TxtHasil.Text = TxtHasil.Text + "0"
+        End If
+
+
+    End Sub
+
+    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+
+        operasi = "tambah"
+        Hasil = Val(TxtHasil.Text)
+        operasiHitung = True
+
+    End Sub
+
+    Private Sub BtnKurang_Click(sender As Object, e As EventArgs) Handles BtnKurang.Click
+
+        operasi = "kurang"
+        Hasil = Val(TxtHasil.Text)
+        operasiHitung = True
+
+    End Sub
+
+    Private Sub BtnKali_Click(sender As Object, e As EventArgs) Handles BtnKali.Click
+
+        operasi = "kali"
+        Hasil = Val(TxtHasil.Text)
+        operasiHitung = True
+
+    End Sub
+
+    Private Sub BtnBagi_Click(sender As Object, e As EventArgs) Handles BtnBagi.Click
+
+        operasi = "bagi"
+        Hasil = Val(TxtHasil.Text)
+        operasiHitung = True
+
+    End Sub
+
+    Private Sub BtnEquals_Click(sender As Object, e As EventArgs) Handles BtnEquals.Click
+
+        Select Case operasi
+            Case "tambah"
+                Hasil += Val(TxtHasil.Text)
+            Case "kurang"
+                Hasil -= Val(TxtHasil.Text)
+            Case "kali"
+                Hasil *= Val(TxtHasil.Text)
+            Case "bagi"
+                Hasil /= Val(TxtHasil.Text)
+        End Select
+
+        TxtHasil.Text = Hasil
+        operasiHitung = True
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+
+        TxtHasil.Text = "0"
+        operasi = ""
+
+    End Sub
+
+    Private Sub BtnClearEntry_Click(sender As Object, e As EventArgs) Handles BtnClearEntry.Click
+
+        If TxtHasil.Text.Length <> 0 Then
+            TxtHasil.Text = TxtHasil.Text.Remove(TxtHasil.Text.Length - 1)
+        End If
+
+    End Sub
+End Class
